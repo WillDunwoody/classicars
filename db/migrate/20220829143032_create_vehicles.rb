27055a352ae21efa00af18a7a558ea275788d977
Kurt Fraser <kurtfra@hotmail.com>
2022-08-29 16:36:16 +0200

    Created vehicle model

---
+++ db/migrate/20220829143032_create_vehicles.rb
@@ -0,0 +1,17 @@
+class CreateVehicles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :vehicles do |t|
+      t.string :model
+      t.string :make
+      t.integer :year
+      t.string :type
+      t.integer :passengers
+      t.text :description
+      t.string :location
+      t.float :price
+      t.references :users, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
