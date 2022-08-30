efbe6bd67ddf38e709f1fa2fef7eedbb2f14b68d
Kurt Fraser <kurtfra@hotmail.com>
2022-08-30 18:00:03 +0200

    Vaildations complete

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :vehicle
+  validates :start_date, presence: true, comparison: {
+    less_than_or_equal_to: :end_date, message: "Start date must be less then end date"
+  }
+  validates :end_date, presence: true, comparison: {
+    greater_than_or_equal_to: :start_date, message: "End date must be greater then start date"
+  }
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,4 +2,15 @@ class Vehicle < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :users, through: :bookings
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :year, presence: true, numericality: { only_integer: true }
+  validates :vehicle_type, presence: true
+  validates :passengers, presence: true, numericality: { only_integer: true }
+  validates :description, presence: true, length: {
+    minimum: 100,
+    too_short: "Your description is too short, min 100 characters"
+  }
+  validates :location, presence: true
+  validates :price, presence: true
 end
