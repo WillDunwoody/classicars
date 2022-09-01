33e148a3058dd559d4abf6e12d5ee2f4bd504e62
will dunwoody <will.dunwoody@gmail.com>
2022-09-01 15:01:34 +0200

    added review functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,13 +9,18 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @vehicle = Vehicle.find(params[:vehicle_id])
+    @review.vehicle = @vehicle
+    @review.user = current_user
+    @review.save
 
     authorize @review
+
+    redirect_to user_path(current_user)
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content, :rating)
+    params.require(:review).permit(:comment, :rating)
   end
 end
