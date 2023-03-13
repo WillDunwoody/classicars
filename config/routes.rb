bd65b1211b7fc2bf69691f668fbd67c3c3cfd144
will dunwoody <will.dunwoody@gmail.com>
2023-03-13 11:40:01 +0100

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
 
-  resources :users, only: [:show] do
-    resources :vehicles, only: [:new, :create]
-  end
-
+  resources :users, only: [:show]
+  resources :vehicles, only: [:new, :create]
 end
